5f3478f50b9943a0cccaf03486502884dab8d5fa
mj13002600 <endoftheworld801@gmail.com>
2026-03-15 19:13:00 +0900

    修正

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -4,16 +4,10 @@ public class DictionaryExec_Chapter21 {
 
     public static void main(String[] args) {
 
-        Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+        Dictionary_Chapter21 dictionaryChapter21 = new Dictionary_Chapter21();
 
         String[] words = {"apple", "banana", "grape", "orange"};
-
-        for (String word : words) {
-            if (dictionary.dictionary.containsKey(word)) {
-                System.out.println(word + "の意味は" + dictionary.dictionary.get(word));
-            } else {
-                System.out.println(word + "は辞書に存在しません。");
-            }
-        }
+        dictionaryChapter21.referer(words);
+        
     }
 }
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -4,20 +4,30 @@ import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
 
-    // 英単語 → 日本語 の辞書
-    public HashMap<String, String> dictionary = new HashMap<>();
+	// 英単語 → 日本語 の辞書
+	private HashMap<String, String> dictionary = new HashMap<>();
 
-    // コンストラクタで10個の単語を登録
-    public Dictionary_Chapter21() {
-        dictionary.put("apple", "りんご");
-        dictionary.put("peach", "桃");
-        dictionary.put("banana", "バナナ");
-        dictionary.put("lemon", "レモン");
-        dictionary.put("pear", "梨");
-        dictionary.put("kiwi", "キウィ");
-        dictionary.put("strawberry", "いちご");
-        dictionary.put("grape", "ぶどう");
-        dictionary.put("muscat", "マスカット");
-        dictionary.put("cherry", "さくらんぼ");
-    }
+	
+	public Dictionary_Chapter21() {
+		dictionary.put("apple", "りんご");
+		dictionary.put("peach", "桃");
+		dictionary.put("banana", "バナナ");
+		dictionary.put("lemon", "レモン");
+		dictionary.put("pear", "梨");
+		dictionary.put("kiwi", "キウィ");
+		dictionary.put("strawberry", "いちご");
+		dictionary.put("grape", "ぶどう");
+		dictionary.put("muscat", "マスカット");
+		dictionary.put("cherry", "さくらんぼ");
+	}
+
+	public void referer(String[] words) {
+        for (String word : words) {
+            if (dictionary.containsKey(word)) {
+                System.out.println(word + " の意味は「" + dictionary.get(word) + "」です。");
+            } else {
+                System.out.println(word + " は辞書に登録されていません。");
+            }
+	    }
+	}
 }
